f5200e719e98397ceac01df5fd106b182fa89c33
starrett67 <starrett67@gmail.com>
2017-10-14 22:01:20 -0400

    Create some tests
    -Created api tests with pytest and the tests were running very very slow so I created more tests with chakramjs
    
    -Initial setup of circle ci build

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 import sqlite3 as lite
 import scraper
+from exception_handler import RestfulException
+
 
 class PhoneDataLayer:
     def __init__(self, db='numbers.db'):
@@ -9,7 +11,8 @@ class PhoneDataLayer:
             with con:
                 cur = con.cursor()
                 cur.execute("DROP TABLE IF EXISTS Numbers")
-                cur.execute("CREATE TABLE Numbers(number TEXT primary key not null, count INT, comment TEXT, date TEXT)")
+                cur.execute(
+                    "CREATE TABLE Numbers(number TEXT primary key not null, count INT, comment TEXT, date TEXT)")
         except lite.Error, e:
             raise e
         finally:
@@ -21,15 +24,17 @@ class PhoneDataLayer:
         with con:
             cur = con.cursor()
             for entry in entries:
-                cur.execute('INSERT OR REPLACE INTO Numbers(number, count, comment, date) VALUES (?, ?, ?, CURRENT_TIMESTAMP);', [entry.phone_number, entry.report_count, entry.comment])
+                cur.execute('INSERT OR REPLACE INTO Numbers(number, count, comment, date) VALUES (?, ?, ?, CURRENT_TIMESTAMP);', [
+                            entry.phone_number, entry.report_count, entry.comment])
         con.close()
 
     def get_all_entries(self):
-        parser  = scraper.Parser(scraper.ValidUAOpener().open(scraper.PHONE_SITE).read())
+        parser = scraper.Parser(
+            scraper.ValidUAOpener().open(scraper.PHONE_SITE).read())
         entries = parser.parse()
         self.insert_entries(entries)
 
-        con  = lite.connect('numbers.db')
+        con = lite.connect('numbers.db')
         rows = None
         with con:
             cur = con.cursor()
@@ -38,35 +43,41 @@ class PhoneDataLayer:
 
         entries = []
         for row in rows:
-            entries[len(entries):] = [scraper.PhoneNumberEntry(row[0], row[1], row[2])]
+            entries[len(entries):] = [
+                scraper.PhoneNumberEntry(row[0], row[1], row[2])]
 
         con.close()
         return entries
 
     def get_db_entries(self, n=60):
-        con  = lite.connect('numbers.db')
+        con = lite.connect('numbers.db')
         rows = None
         with con:
             cur = con.cursor()
-            cur.execute('SELECT * FROM Numbers ORDER BY date LIMIT {}'.format(n))
+            cur.execute(
+                'SELECT * FROM Numbers ORDER BY date LIMIT {}'.format(n))
             rows = cur.fetchall()
 
         entries = []
         for row in rows:
-            entries[len(entries):] = [scraper.PhoneNumberEntry(row[0], row[1], row[2])]
+            entries[len(entries):] = [
+                scraper.PhoneNumberEntry(row[0], row[1], row[2])]
 
         con.close()
         return entries
 
     def get_entries(self, n=None):
-        parser  = scraper.Parser(scraper.ValidUAOpener().open(scraper.PHONE_SITE).read())
+        parser = scraper.Parser(
+            scraper.ValidUAOpener().open(scraper.PHONE_SITE).read())
         entries = parser.parse()
         self.insert_entries(entries)
 
-        if n is None:
+        if n is None or n < 0:
             return entries
         elif n < len(entries):
             return entries[:n]
         else:
-            return self.get_db_entries(n)
-
+            try:
+                return self.get_db_entries(n)
+            except:
+                raise RestfulException('Invalid count given', status_code=400)
